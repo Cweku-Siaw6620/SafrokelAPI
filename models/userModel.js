59866966d67224492698292c14fa0d4c60b19766
Kelvin Ashong <kksashong@st.ug.edu.gh>
2025-09-15 01:54:31 +0000

    Created basic endpoints to save user info

---
+++ models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  phoneNumber: { type: String, required: true, unique: true },
+  password: { type: String, required: false },
+});
+
+userSchema.pre('save', async function (next) {
+    try {
+        // Only hash the password if it is new or modified
+        if (!this.isModified('password')) return next();
+
+        // Generate a salt and hash the password
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = mongoose.model("Users",userSchema);
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const port = process.env.PORT || 3000;
+
+//importing the models
+const User = require('./models/userModel');
+
+const app = express();
+
+//middleware
+app.use(cors())
+app.use(express.json());
+
+//saving users accounts
+app.post("/users", async(req,res)=>{
+    let user = new User(req.body);
+    let result = await user.save();
+    res.send(result);
+})
+  
+//fetching user accounts
+app.get('/users' , async(req,res) =>{
+    try {
+         const users = await User.find({});
+         res.status(200).json(users);
+    } catch (error) {
+         console.log(error.message);
+         res.status(500).json({message: error.message});
+    }
+ })
+ 
+//deleting user account
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findByIdAndDelete(id);  // Find the product by ID and delete it
+
+        if (!user) {
+            return res.status(404).json({ message: `No user found with ID ${id}` });
+        }
+
+        res.status(200).json({ message: `User with ID ${id} has been deleted` });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+mongoose.connect('mongodb+srv://kelvinashong02_db_user:safrokelVentures@safrokelventures.p2obcx2.mongodb.net/?retryWrites=true&w=majority&appName=SafrokelVentures')
+.then(()=>{
+    console.log("connected to Safrokel mongodb");
+    app.listen(port, ()=>{
+        console.log('Safrokel API is running on port ' + port);
+    })
+}).catch(()=>{
+    console.log(error);
+})
+
+
